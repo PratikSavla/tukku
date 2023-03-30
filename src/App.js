130cc6ac7cf837cbb9e1856363676e2352adc049
PratikSavla <pratiksavla07@gmail.com>
2023-03-30 23:07:05 +0530

    for ♥

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Grid, Paper, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import ContainerMain from "./components/ContainerMain";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Grid container spacing={2} color={"gray"}>
+        <Grid item xs={12}>
+        <Navbar />
+        </Grid>
+        <Grid item xs={8}>
+          <Paper>
+          <Grid container spacing={2}>
+          {
+            [1,2,3,4,5].map(i => { return (
+            <Grid item xs={4}><ContainerMain data={i} key={i}/>
+            </Grid>
+            )})
+          }
+          </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper>
+          <Typography>Hello</Typography>
+          </Paper>
+        </Grid>
+      </Grid>
   );
 }
 
---
+++ src/components/ContainerMain.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, Container, Paper, Typography } from "@mui/material";
+
+function ContainerMain() {
+  return (
+    <Card variant="outlined">Helllo</Card>
+  )
+}
+export default ContainerMain;
